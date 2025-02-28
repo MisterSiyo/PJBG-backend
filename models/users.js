f8ced3b28f6a7da6c4dfc64f5ac4103070b9ca79
Your nameLaura <laurabouraouipoussin@gmail.com>
2025-02-28 18:10:12 +0100

    users

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// Schéma pour les adresses (=sous document de user)
 const adressSchema = new mongoose.Schema(
     { 
         street: { type: String, required: true }, 
@@ -9,6 +10,7 @@ const adressSchema = new mongoose.Schema(
     },
 )
 
+// Schéma pour les réseaux sociaux (=sous document de user)
 const socialLinksSchema = new mongoose.Schema(
         {
             platform: { 
@@ -19,8 +21,100 @@ const socialLinksSchema = new mongoose.Schema(
             }
 )
 
-const userSchema = new mongoose.Schema(
+// Schéma pour les personne de contact du studio (=sous document de user)
+const contactPersonSchema = new mongoose.Schema(
+    { 
+        name: { type: String, required: true }, 
+        surname: { type: String, required: true }, 
+        email: { type: String, required: true }, 
+        phone: { type: String, required: true } 
+    },
+)
+
+// Schéma pour les managers du studio (=sous document de user)
+const contactManagerSchema = new mongoose.Schema(
+    { 
+        name: { type: String, required: true }, 
+        surname: { type: String, required: true }, 
+        email: { type: String, required: true }, 
+        phone: { type: String, required: true } 
+    },
+)
+ // Liste des projets que l'utilisateur suit (mais n'a pas forcément financé) (=sous document de user)
+const fundedProjectsSchema = new mongoose.Schema(
     {
+      project: 
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'projects',
+        },
+      pledgeChosen: 
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'pledges',
+        },
+    }
+)
+
+// Schéma des infos spécifiques aux studios (=sous document de user)
+const studioSchema = new mongoose.Schema(
+    {
+        siret:{
+            type: String,
+            required: true,  
+            unique: true,
+        },
+        webSite: {
+            type: String,
+            required: true, 
+            unique: true,
+        },
+        companyName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        brand: { 
+            type: String
+        }, 
+        subBrand: { 
+            type: String
+        }, 
+        contactPerson: [
+           contactPersonSchema
+        ],
+        contactManager: [
+            contactManagerSchema
+        ],
+        chosenProjects: [ // Liste des projets auquels le studio à postulé
+            {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'projects'
+            }
+        ],
+        developedProjects: [ // Liste des projets dévelopés / en cours de developpement par le studio
+            {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'projects'
+            }
+        ],
+        followedProjects: [ // Liste des projets que le studio suit sans forcément les avoir pris en charge pour dev
+            {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'projects'
+            }
+        ],
+}
+);
+
+// Schéma des users (= studios + backers)
+const userSchema  = new mongoose.Schema(
+    {
+        studio: studioSchema,
         username: {
             type: String,
             required: true,
@@ -43,35 +137,75 @@ const userSchema = new mongoose.Schema(
         },
         role: { 
             type: String, 
-            enum: ['user', 'studio'], // Se renseigne automatiquement en focntion de la création de compte
+            enum: ['backers', 'studio'], // Se renseigne automatiquement en focntion de la création de compte
             required: true,
         },
         socialLinks: [
             socialLinksSchema
         ],
-         address: [
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        surname: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        address: [
             adressSchema
          ],
-         fundedProjects: [ // Liste des projets financés par l'utilisateur
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        preferences: // Genre de jeux préférés
+        [ 
+             {
+              type: mongoose.Schema.Types.ObjectId, 
+              ref: 'gameMechanics'
+             }
+        ],
+        secondChoices: // Genre de jeux aimés (second choix)
+        [ 
             {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Project'
+             type: mongoose.Schema.Types.ObjectId, 
+             ref: 'gameMechanics'
             }
         ],
+        restrictions: // Genre de jeux à bannir
+        [ 
+            {
+             type: mongoose.Schema.Types.ObjectId, 
+             ref: 'gameMechanics'
+            }
+       ],
+        fundedProjects: fundedProjectsSchema, // Liste des projets financé par l'utilisateur
         followedProjects: [ // Liste des projets que l'utilisateur suit (mais n'a pas forcément financé)
             {
             type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Project'
+            ref: 'projects'
             }
         ],
-        createdProjects: [ // Liste des projets créés par l'utilisateur (il a financé et créé)
+        createdProjects: [ // Liste des projets créés par l'utilisateur (qu'il a financé et créé)
         {type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Project'}
+        ref: 'projects'}
         ],
 },
     { timestamps: true }
 );
 
+
+// Vérification : studioInfo obligatoire si role = "studio"
+userSchema.pre('save', function (next) {
+    if (this.role === 'studio' && !this.studioInfo) {
+        return next(new Error('Les studios doivent avoir un studioInfo rempli.'));
+    }
+    next();
+});
+
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = { User};
